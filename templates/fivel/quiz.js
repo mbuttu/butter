bc82089ef8dc9ba08895d5d24480d10bf6ca0c79
Mohammed Buttu <mohammedbuttu@gmail.com>
2012-08-22 22:32:59 -0400

    Refactor table of contents plugin/editor
    Table of contents (toc) is no longer a plugin, but has an editor that lives on
    its own that can specify the sections in the table of contents, as well
    as the target.
    
    The reason that this needs to be done is because plugins cannot remove
    cues.
    
    * toc editor can be opened from the template.
    * Sections can be specified in the config file.
    * Highlighting of the toc works when the times are changed.
    * Highlighting of the toc works when the user seeks the video.
    * Sections are sorted every time there is a change.
    * Last section is marked complete when the video ends.

--- templates/fivel/quiz.js
+++ templates/fivel/quiz.js
@@ -1,5 +1,11 @@
 document.addEventListener( "DOMContentLoaded", function( e ){
   Butter({
-    config: 'config.json'
+    config: 'config.json',
+    ready: function( butter ){
+      var tocButton = document.getElementById( "opentoc" );
+      tocButton.addEventListener( "click", function( e ) {
+        butter.editor.openEditor( "toc", false, {} );
+      });
+    }
   });
 }, false );
