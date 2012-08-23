326a5f5dd5bcfdd907a91c500b0c0ee18b2d5596
Mohammed Buttu <mohammedbuttu@gmail.com>
2012-08-23 08:53:14 -0400

    Show toc on load, and rename quiz.js to ready.js

--- templates/fivel/ready.js
+++ templates/fivel/ready.js
@@ -2,6 +2,9 @@ document.addEventListener( "DOMContentLoaded", function( e ){
   Butter({
     config: 'config.json',
     ready: function( butter ){
+      // Force open and close the toc editor so that the table of contents will show when the page is laoded
+      butter.editor.openEditor( "toc", false, {} );
+      butter.editor.openEditor( "plugin-list", true );
       var tocButton = document.getElementById( "opentoc" );
       tocButton.addEventListener( "click", function( e ) {
         butter.editor.openEditor( "toc", false, {} );
