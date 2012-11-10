ad02275b3b8561c715ba53c090b550a6e8c00497
Mohammed Buttu <mohammedbuttu@gmail.com>
2012-11-10 17:12:07 -0500

    Add private fivel template

---
+++ templates/fivel/private/ready.js
@@ -0,0 +1,64 @@
+(function ( Butter ){
+  document.addEventListener( "DOMContentLoaded", function( e ){
+    Butter.init({
+      config: "config.json",
+      ready: function( butter ){
+        butter.listen( "editoropened", function onEditorOpened() {
+          butter.unlisten("editoropened", onEditorOpened);
+          var trackEvent = butter.getTrackEventsByType( "toc" )[ 0 ],
+              tocPlugin = document.querySelector( "[data-popcorn-plugin-type=toc]" ),
+              popcornString = "";
+
+          function sendToServer( e ){
+            $.ajax({
+              type: "POST",
+              url: "http://localhost:3000/template",
+              data: {
+                script: encodeURIComponent( popcornString )
+              }
+            }).
+            done(function( data ) {
+              butter.dialog.spawn( "share", {
+                data: data.url
+              }).open();
+            });
+          }
+
+          if ( trackEvent ) {
+            // hide the toc track event so that the editor can only be opened
+            // from the template
+            trackEvent.view.element.style.display = "none";
+            // hide the toc plugin from the plugin-list editor so that only one
+            // toc can be added to a project
+            tocPlugin.style.display = "none";
+          }
+
+          var tocButton = document.getElementById( "opentoc" );
+          tocButton.addEventListener( "click", function( e ) {
+            butter.editor.openEditor( "toc", {
+              openData: trackEvent
+            });
+          });
+
+          var exportButton = document.getElementById( "export" );
+          exportButton.addEventListener( "click", function( e ) {
+            popcornString = "(function() {\n" +
+                                  butter.currentMedia.generatePopcornString() + "\n" +
+                               "})();\n";
+            butter.dialog.spawn( "export", {
+              data: {
+                popcornString: popcornString,
+                sendToServerCallback: sendToServer
+              },
+              events: {
+                cancel: function( e ){
+                  dialg.close();
+                }
+              }
+            }).open();
+          });
+      });
+      }
+    });
+  }, false );
+}( window.Butter ));
