d1cd8ee32b1c8554262461a20088f0ba9172bdd8
Matthew Schranz <schranz.m@gmail.com>
2012-12-04 17:44:04 -0500

    Version one slider plugin.
    Fixes #730

---
+++ templates/fivel/editors/slider.js
@@ -0,0 +1,17 @@
+/*global EditorHelper*/
+
+EditorHelper.addPlugin( "slider", function( trackEvent ) {
+  var _container,
+      _popcornOptions,
+      target;
+
+  _popcornOptions = trackEvent.popcornTrackEvent;
+  _container = _popcornOptions._container;
+  target = _popcornOptions._target;
+
+  if ( window.jQuery ) {
+
+    window.EditorHelper.draggable( trackEvent, _container, target );
+  }
+
+});
