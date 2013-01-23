c39c2f0037cdda07982c7aa64b36259dee1e4654
Mohammed Buttu <mohammedbuttu@gmail.com>
2013-01-23 14:47:49 -0500

    Use text plugin from Butter instead of the default popcorn one

---
+++ templates/fivel/editors/quiz.js
@@ -0,0 +1,18 @@
+/*global EditorHelper*/
+
+EditorHelper.addPlugin( "quiz", function( trackEvent ) {
+  debugger;
+  var _container,
+      _popcornOptions,
+      target;
+
+  _popcornOptions = trackEvent.popcornTrackEvent;
+  _container = _popcornOptions._container;
+  target = _popcornOptions._target;
+
+  if ( window.jQuery ) {
+
+    window.EditorHelper.draggable( trackEvent, _container, target );
+  }
+
+});
